ef8d78c24da7651b8e1fbef5d6970e33c813c69c
Garrett Brady Hansen <hansengb@s.dcsdk12.org>
2019-04-02 07:42:21 -0600

    HansenA1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,12 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card jeff = new Card("Ace", "Spades", 1);
+		Card jefe = new Card("Queen", "Clubs", 12);
+		Card jeef = new Card("Ace", "Spades", 1);
+
+		System.out.println(jeff.matches(jeef));
+		System.out.println(jefe.toString());
+		
 	}
 }
